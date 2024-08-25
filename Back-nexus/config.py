fc43a7a0a27a10a40a77502cebd98b5c20e88a98
Joël Mamilalaina <98610687+ISS19@users.noreply.github.com>
2024-08-26 00:24:40 +0300

    Delete Back-nexus/config.py

--- Back-nexus/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
-    MONGO_URI = os.getenv(
-        "MONGO_URI",
-        "mongodb+srv://iss19:boobadkr123@cluster0.yhsfrfj.mongodb.net/",
-    )
