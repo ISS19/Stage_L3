0766c0a8ae0eb9a48b6d954e0405bd7af9fe9bda
Iss best <joel.sfyritech@pm.me>
2024-09-17 17:04:54 +0300

    front

--- Front-nexus/src/pages/inscription/index.tsx
+++ Front-nexus/src/pages/inscription/index.tsx
@@ -1,118 +1,138 @@
 import React, { useState } from "react";
 import styles from "@/styles/Inscription.module.scss";
+import Link from "next/link";
 
 function Inscription() {
-  const [currentStep, setCurrentStep] = useState(1); // Gère l'étape actuelle de l'inscription
+  const [currentStep, setCurrentStep] = useState(1);
 
-  // Fonction pour passer à l'étape suivante
   const handleNext = () => {
     if (currentStep < 2) {
       setCurrentStep(currentStep + 1);
     }
   };
 
-  // Fonction pour revenir à l'étape précédente
   const handlePrevious = () => {
     if (currentStep > 1) {
       setCurrentStep(currentStep - 1);
     }
   };
 
-  // Fonction pour soumettre le formulaire
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Logique pour la soumission du formulaire
     console.log("Form submitted");
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.titleBox}>
-        <h1 className={styles.title}>AID-AUTH</h1>
-        <img
-          src="/3dDoctor.png"
-          alt="Doctor"
-          className={`${styles.imgDoctor} ${styles.desktop}`}
-        />
-      </div>
-
-      <div className={styles.loginBox}>
-        <div className={styles.title}>
-          <h1>S'inscrire</h1>
-          <p>Inscrivez vos informations personnelles</p>
+    <>
+      <Link href="/">
+        <img src="/aid-logo.png" alt="" className={styles.logo} />
+      </Link>
+      <div className={styles.container}>
+        <div className={styles.titleBox}>
+          <h1 className={styles.title}>AID-AUTH</h1>
+          <img
+            src="/online-doctor-animate.svg"
+            alt="Doctor"
+            className={`${styles.imgDoctor} ${styles.desktop}`}
+          />
         </div>
-        <br />
-        <br />
-        <form onSubmit={handleSubmit}>
-          {currentStep === 1 && (
-            <div className={styles.step}>
-              {/* Première section d'inscription */}
-              <div className={styles.formGroup}>
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
-              </div>
 
-              <div className={styles.formGroup}>
-                <label htmlFor="password">Mot de passe:</label>
-                <input type="password" id="password" name="password" required />
+        <div className={styles.loginBox}>
+          <div className={styles.title}>
+            <h1>S'inscrire</h1>
+            <p>Inscrivez vos informations personnelles</p>
+          </div>
+          <br />
+          <br />
+          <form onSubmit={handleSubmit}>
+            {currentStep === 1 && (
+              <div className={styles.step}>
+                {/* Première section d'inscription */}
+                <div className={styles.formGroup}>
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" id="email" name="email" required />
+                </div>
+
+                <div className={styles.formGroup}>
+                  <label htmlFor="password">Mot de passe:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                  />
+                </div>
+              </div>
+            )}
+
+            {currentStep === 2 && (
+              <div className={styles.step}>
+                {/* Deuxième section d'inscription */}
+                <div className={styles.formGroup}>
+                  <label htmlFor="firstName">Prénom:</label>
+                  <input type="text" id="firstName" name="firstName" required />
+                </div>
+
+                <div className={styles.formGroup}>
+                  <label htmlFor="lastName">Nom:</label>
+                  <input type="text" id="lastName" name="lastName" required />
+                </div>
+              </div>
+            )}
+
+            <br />
+
+            <div className={styles.formGroup}>
+              <div className={styles.buttonGroup}>
+                {currentStep > 1 && (
+                  <button
+                    type="button"
+                    className={styles.button}
+                    onClick={handlePrevious}
+                  >
+                    Précédent
+                  </button>
+                )}
+
+                {currentStep < 2 ? (
+                  <button
+                    type="button"
+                    className={styles.button}
+                    onClick={handleNext}
+                  >
+                    Suivant
+                  </button>
+                ) : (
+                  <button type="submit" className={styles.button}>
+                    Valider
+                  </button>
+                )}
               </div>
             </div>
-          )}
+          </form>
 
-          {currentStep === 2 && (
-            <div className={styles.step}>
-              {/* Deuxième section d'inscription */}
-              <div className={styles.formGroup}>
-                <label htmlFor="firstName">Prénom:</label>
-                <input type="text" id="firstName" name="firstName" required />
-              </div>
+          <p className={styles.linkSignin}>
+            Déjà inscrit?{" "}
+            <Link href="/login" className={styles.loginLink}>
+              Connectez-vous ici
+            </Link>
+          </p>
 
-              <div className={styles.formGroup}>
-                <label htmlFor="lastName">Nom:</label>
-                <input type="text" id="lastName" name="lastName" required />
-              </div>
-            </div>
-          )}
-
+          <img
+            src="/online-doctor-animate.svg"
+            alt="Doctor"
+            className={`${styles.imgDoctor} ${styles.mobile}`}
+          />
           <br />
 
-          <div className={styles.formGroup}>
-            {/* Bouton Précédent, visible uniquement à partir de l'étape 2 */}
-            {currentStep > 1 && (
-              <button
-                type="button"
-                className={styles.button}
-                onClick={handlePrevious}
-              >
-                Précédent
-              </button>
-            )}
-
-            {/* Bouton Suivant ou Valider selon l'étape */}
-            {currentStep < 2 ? (
-              <button type="button" className={styles.button} onClick={handleNext}>
-                Suivant
-              </button>
-            ) : (
-              <button type="submit" className={styles.button}>
-                Valider
-              </button>
-            )}
-          </div>
-        </form>
-
-        <img
-          src="/3dDoctor.png"
-          alt="Doctor"
-          className={`${styles.imgDoctor} ${styles.mobile}`}
-        />
-        <br />
-
-        <footer className={styles.footer}>
-          <p>&copy; {new Date().getFullYear()} AID-NEXUS. Pour vous servir.</p>
-        </footer>
+          <footer className={styles.footer}>
+            <p>
+              &copy; {new Date().getFullYear()} AID-NEXUS. Pour vous servir.
+            </p>
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- Front-nexus/src/pages/login/index.tsx
+++ Front-nexus/src/pages/login/index.tsx
@@ -1,68 +1,83 @@
 import React from "react";
 import styles from "@/styles/Login.module.scss";
+import Link from "next/link";
 
 function Login() {
   return (
-    <div className={styles.container}>
-      <div className={styles.titleBox}>
-        <h1 className={styles.title}>AID-AUTH</h1>
-        <img
-          src="/3dDoctor.png"
-          alt="Doctor"
-          className={`${styles.imgDoctor} ${styles.desktop}`}
-        />
-      </div>
-
-      <div className={styles.loginBox}>
-        <div className={styles.title}>
-          <h1>Se connecter</h1>
-          <p>Connecter vous pour consulter le docteur</p>
+    <>
+      <Link href="/">
+        <img src="/aid-logo.png" alt="" className={styles.logo} />
+      </Link>
+      <div className={styles.container}>
+        <div className={styles.titleBox}>
+          <h1 className={styles.title}>AID-AUTH</h1>
+          <img
+            src="/online-doctor-animate.svg"
+            alt="Doctor"
+            className={`${styles.imgDoctor} ${styles.desktop}`}
+          />
         </div>
-        <br />
-        <br />
-        <form>
-          <div className={styles.formGroup}>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-          </div>
 
-          <div className={styles.formGroup}>
-            <label htmlFor="password">Mot de passe:</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-
-          <div className={styles.formGroup}>
-            <div className={styles.rememberAndForgot}>
-              <div className={styles.rememberMe}>
-                <input type="checkbox" id="rememberMe" name="rememberMe" />
-                <label htmlFor="rememberMe">Rester connecté</label>
-              </div>
-              <a href="#" className={styles.forgotPassword}>
-                Forgot Password?
-              </a>
-            </div>
+        <div className={styles.loginBox}>
+          <div className={styles.title}>
+            <h1>Se connecter</h1>
+            <p>Connecter vous pour consulter le docteur</p>
           </div>
           <br />
+          <br />
+          <form>
+            <div className={styles.formGroup}>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
 
-          <div className={styles.formGroup}>
-            <button type="submit" className={styles.button}>
-              Se connecter
-            </button>
-          </div>
-        </form>
+            <div className={styles.formGroup}>
+              <label htmlFor="password">Mot de passe:</label>
+              <input type="password" id="password" name="password" required />
+            </div>
 
-        <img
-          src="/3dDoctor.png"
-          alt="Doctor"
-          className={`${styles.imgDoctor} ${styles.mobile}`}
-        />
-        <br />
+            <div className={styles.formGroup}>
+              <div className={styles.rememberAndForgot}>
+                <div className={styles.rememberMe}>
+                  <input type="checkbox" id="rememberMe" name="rememberMe" />
+                  <label htmlFor="rememberMe">Rester connecté</label>
+                </div>
+                <a href="#" className={styles.forgotPassword}>
+                  Forgot Password?
+                </a>
+              </div>
+            </div>
+            <br />
 
-        <footer className={styles.footer}>
-          <p>&copy; {new Date().getFullYear()} AID-NEXUS. Pour vous servir.</p>
-        </footer>
+            <div className={styles.formGroup}>
+              <button type="submit" className={styles.button}>
+                Se connecter
+              </button>
+            </div>
+          </form>
+
+          <p className={styles.linkSignup}>
+            Pas encore inscrit?{" "}
+            <Link href="/inscription" className={styles.registerLink}>
+              Créez un compte ici
+            </Link>
+          </p>
+
+          <img
+            src="/online-doctor-animate.svg"
+            alt="Doctor"
+            className={`${styles.imgDoctor} ${styles.mobile}`}
+          />
+
+          <br />
+          <footer className={styles.footer}>
+            <p>
+              &copy; {new Date().getFullYear()} AID-NEXUS. Pour vous servir.
+            </p>
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
